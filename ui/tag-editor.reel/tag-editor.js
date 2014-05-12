4000960b942a57f44c7c0502fd9ada0ef0373d6b
Michel Mongkhoy <noreply>
2014-05-12 16:34:16 +0000

    Add component tag-editor

---
+++ ui/tag-editor.reel/tag-editor.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/tag-editor.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class TagEditor
+ * @extends Component
+ */
+exports.TagEditor = Component.specialize(/** @lends TagEditor# */ {
+    constructor: {
+        value: function TagEditor() {
+            this.super();
+        }
+    }
+});
