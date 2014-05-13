97f52c38b11dcfb62f3da2fb66896e64816fbdf5
Michel Mongkhoy <noreply>
2014-05-13 20:26:19 +0000

    Add component tag-text-field

---
+++ ui/tag-text-field.reel/tag-text-field.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/tag-text-field.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class TagTextField
+ * @extends Component
+ */
+exports.TagTextField = Component.specialize(/** @lends TagTextField# */ {
+    constructor: {
+        value: function TagTextField() {
+            this.super();
+        }
+    }
+});
