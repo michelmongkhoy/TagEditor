010becedf1903569e6bd46e5a90f12251d1fe521
Michel Mongkhoy <noreply>
2014-05-13 16:01:32 +0000

    Add component tag-button

---
+++ ui/tag-button.reel/tag-button.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/tag-button.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class TagButton
+ * @extends Component
+ */
+exports.TagButton = Component.specialize(/** @lends TagButton# */ {
+    constructor: {
+        value: function TagButton() {
+            this.super();
+        }
+    }
+});
