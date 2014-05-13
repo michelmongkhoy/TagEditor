c613aa4eae9895200bea13d1bb191ad808ab2eab
Michel Mongkhoy <noreply>
2014-05-13 16:05:40 +0000

    Update component /ui/tag-button.reel/tag-button.js

--- ui/tag-button.reel/tag-button.js
+++ ui/tag-button.reel/tag-button.js
@@ -3,12 +3,12 @@
  * @requires montage/ui/component
  */
 var Component = require("montage/ui/component").Component;
-
+var AbstractButton = require("montage/ui/base/abstract-button").AbstractButton;
 /**
  * @class TagButton
  * @extends Component
  */
-exports.TagButton = Component.specialize(/** @lends TagButton# */ {
+exports.TagButton = AbstractButton.specialize(/** @lends TagButton# */ {
     constructor: {
         value: function TagButton() {
             this.super();
